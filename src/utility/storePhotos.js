1e43f187b9716ab3d80f98b9ae6393230930b8ae
Marcin <marcinwarzybok@outlook.com>
2020-09-17 15:13:56 +0200

    working file downloading

--- src/utility/storePhotos.js
+++ src/utility/storePhotos.js
@@ -7,34 +7,55 @@ import path from "path";
 
 //longitude, latitude
 const points = [
-  [19.9245235, 50.059255],
-  [19.9242931, 50.0592544],
-  [19.9241684, 50.0592645],
-  [19.9239091, 50.059296],
+  [20.4132983, 49.4264564],
+  [20.4143716, 49.4272136],
+  [20.4153793, 49.4277218],
+  [20.4157706, 49.4277918],
+  [20.4158422, 49.4278046],
+  [20.4163634, 49.4278978],
 ];
 
-var f = fs.createWriteStream("test.jpeg");
+const downloadArray = [];
 
-const writer = fs.createWriteStream("./test.jpeg");
-axios({
-  url: `https://maps.googleapis.com/maps/api/streetview?key=${process.env.GOOGLE_MAPS_API_KEY}&size=1200x900&location=47.5763831,-122.4211769&heading=80`,
-  responseType: "stream",
-})
-  .then((response) => {
-    response.data.pipe(writer);
-    let error = null;
-    writer.on("error", (err) => {
-      error = err;
-      writer.close();
-    });
-    writer.on("close", () => {
-      if (!error) {
-        console.log("Udało się !!!!");
-      }
-      //no need to call the reject here, as it will have been called in the
-      //'error' stream;
-    });
-  })
-  .catch((err) => {
-    console.log(err.response.data);
+for (let i = 0; i < points.length; i++) {
+  downloadArray.push(
+    fileDownload(
+      { lat: points[i][1], lng: points[i][0] },
+      `./data/Poland/malapolskie/photos/point${i}.jpeg`
+    )
+  );
+}
+
+Promise.all(downloadArray).then(() => {
+  console.log("Udało się !!!");
+});
+
+function fileDownload({ lat, lng }, location) {
+  return new Promise((resolve, reject) => {
+    const writer = fs.createWriteStream(location);
+    axios({
+      url: `https://maps.googleapis.com/maps/api/streetview?key=${process.env.GOOGLE_MAPS_API_KEY}&size=1200x900&location=${lat},${lng}&heading=300`,
+      responseType: "stream",
+    })
+      .then((response) => {
+        response.data.pipe(writer);
+        let error = null;
+        writer.on("error", (err) => {
+          error = err;
+          writer.close();
+          reject();
+        });
+        writer.on("close", () => {
+          if (!error) {
+            console.log("Udało się !!!!");
+            resolve(true);
+          }
+          //no need to call the reject here, as it will have been called in the
+          //'error' stream;
+        });
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
   });
+}
